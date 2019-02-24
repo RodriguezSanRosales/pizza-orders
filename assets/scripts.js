95d9ea7e3d4ba79c99977b8940efe22dc51b792a
RodriguezSanRosales <san.rodzrosales@gmail.com>
2019-02-24 02:52:18 -0600

    se agregaron estilos y funciones

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,26 +1,76 @@
-console.log('yey!');
-
-
 const pizzaForm = document.querySelector('#pizza-form');
-const pizza = document.querySelector('#pizza');
-const lista = document.querySelector('#lista');
+const sizePizza = pizzaForm.size;
+var checkboxes = document.getElementsByName('ingredients');
 
-console.log(pizzaForm);
+const pizzaMasa = document.getElementById('opcionMasa');
+const pizzaNumbers = document.getElementById('cantidad');
 
+//orders section
+const listOrder = document.querySelector('.list-order');
 
-pizzaForm.addEventListener('submit', function(event){
+var activeOrder = [];
+
+pizzaForm.addEventListener('submit' , function(event){
   event.preventDefault();
-  console.log('Hemos detenido el submit');
-  const saborPizza = pizza.value;
-  console.log(saborPizza);
-  // lista.innerHTML += 'Tu pizza es sabor: ' + saborPizza;
-  lista.innerHTML += `<li>Tu pizza es sabor: ${saborPizza} </li>`;
+
+  const masaValue = pizzaMasa.value;
+  const sizeValue = sizePizza.value;
+  const numberValue = pizzaNumbers.value;
+
+  //checkboxes
+  var vals = "";
+  for (var i=0; i < checkboxes.length;  i++) {
+      if (checkboxes[i].checked) {
+        vals += "<li>" + checkboxes[i].value + '</li>';
+      }
+  }
+
+  const newPizza = {
+    masa: masaValue,
+    size: sizeValue,
+    numbers: numberValue,
+    ingredients: vals
+  }
+  activeOrder.push(newPizza);
+  updateList();
 });
 
+function updateList(){
+  listOrder.innerHTML = `<h2 class="titleOrder">Orden</h2>`;
+  for(let pizza of activeOrder){
+    const contInfo = `
+      <div class="cont-infoPizza">
+        <span class="btn-delete"><i class="fas fa-times"></i></span>
+        <h3>Tipo de Masa: ${pizza.masa}</h3>
+        <h3>Tamaño: ${pizza.size}</h3>
+        <h3>Cantidad de Pizzas: ${pizza.numbers}</h3>
+        <h3>Ingredientes:</h3>
+        <ul> ${pizza.ingredients} </ul>
+      </div>
+    `
+    listOrder.innerHTML += contInfo;
+  }
 
+  var btn = document.getElementsByClassName('btn-delete');
+  for (var i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function(e) {
+      e.currentTarget.parentNode.remove();
 
+    });
+  }
+}
 
+//scroll menu
 
+var header = document.getElementById('header');
+var scrollpos = window.scrollY;
 
-
-//
+window.addEventListener('scroll', function(){ 
+    scrollpos = window.scrollY;
+    if(scrollpos >= 150){
+      header.classList.add("scrollMenu");
+    }
+    else {
+      header.classList.remove("scrollMenu");
+    }
+});
